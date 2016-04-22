ea3e8e73674ef00b4c71c4adb35ff464bcc7553c
Robson Alves <robsonalves.dev@gmail.com>
2016-04-22 00:52:27 -0300

    Criado classe estatica Resposta para auxiliar na resposta da requisicao, convertendo os dados de resposta em Json

--- output/Resposta.php
+++ output/Resposta.php
@@ -3,17 +3,18 @@
 /**
  * Classe de resposta da requisição
  */
-class Requisicao
+class Resposta
 {
 	/**
 	 * Metodo estático que ecoa na tela a resposta em formato Json
 	 * @param  array  $dados [dados de retorno]
 	 * @return [Json]        [Os dados de retorno em formato Json]
 	 */
-	public static function resposta( array $dados )
+	public static function enviar( array $dados )
 	{
 		header('Content-Type: application/json; charset=utf-8');
 		echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+		exit;
 	}
 
 }
