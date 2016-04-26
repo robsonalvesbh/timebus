53feccefaf6674ecf86d51f43e23f85bdac4517e
Robson Alves <robsonalves.dev@gmail.com>
2016-04-25 23:26:03 -0300

    Site TimeBus

---
+++ app/app.js
@@ -0,0 +1,40 @@
+var myApp = angular.module('appBus', [], function($interpolateProvider) {
+   $interpolateProvider.startSymbol('[[');
+   $interpolateProvider.endSymbol(']]');
+});
+
+myApp.controller("MyCtrl", function($scope, $http)
+{
+   $scope.bus = [];
+   $scope.horarios = [];
+
+   $http.get('http://localhost/Projetos/appBus/onibus')
+   .success(function(response) {
+      if (response.status == 200)
+      {
+         $scope.bus = angular.fromJson(response.dados);
+      }
+   }).error(function() {
+      console.error('Arquivo .JSON não encontrado!')
+   });
+
+   $scope.getBus = function( ) {
+      var linha = $scope.onibus.split(" - ");
+
+      $http.get('http://localhost/Projetos/appBus/onibus/'+ linha[0])
+      .success(function(response) {
+         if (response.status == 200)
+         {
+            $scope.horarios = angular.fromJson(response.dados.horarios);
+         }
+      }).error(function() {
+         console.error('Arquivo .JSON não encontrado!');
+      });
+   }
+
+   $scope.download = function( ) {
+      alert('ok');
+   }
+
+});
+
